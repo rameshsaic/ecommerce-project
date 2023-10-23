f3db1785205fbc9339c6f8b227223a4abfbd6321
rameshsaic <144799329+rameshsaic@users.noreply.github.com>
2023-10-23 16:15:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -155,7 +155,7 @@
 
 
                 <div class="copy_right_area">
-                    <h4 class="copy_right">© Copyright 2023 2021ht6658812 Ecommerce | All Rights Reserved</h4>
+                    <h4 class="copy_right">© Copyright 2023 2021ht66588123 Ecommerce | All Rights Reserved</h4>
                 </div>
             </div>
         </footer>
