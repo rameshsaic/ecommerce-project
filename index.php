c1b58ac823c1f4613a41d57a6d88b975b13dee3c
rameshsaic <144799329+rameshsaic@users.noreply.github.com>
2023-10-23 14:48:16 +0100

    test

--- index.php
+++ index.php
@@ -155,7 +155,7 @@
 
 
                 <div class="copy_right_area">
-                    <h4 class="copy_right">© Copyright 2023 2021ht66588444411115 Ecommerce | All Rights Reserved</h4>
+                    <h4 class="copy_right">© Copyright 2023 2021ht66588 Ecommerce | All Rights Reserved</h4>
                 </div>
             </div>
         </footer>
